4bf43fc5b765d6f4af65150cc4aa41e9c08cbd3a
Paulo Cesar Dal Ponte <129686037+pauloDalponte@users.noreply.github.com>
2023-11-14 22:35:34 -0300

    ajuste de telas

--- src/Router/router.tsx
+++ src/Router/router.tsx
@@ -1,10 +1,16 @@
 import { Route, createBrowserRouter, createRoutesFromElements } from "react-router-dom";
 import Login from "../screens/Login";
+import Dashboard from "../screens/Dashboard";
+import Order from "../screens/Order";
+import Stock from "../screens/Stock";
 
 const Router = createBrowserRouter(
     createRoutesFromElements(
         <Route>
-            <Route path="/login" element={<Login />} />
+            <Route path="/" element={<Login />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/order" element={<Order />} />
+            <Route path="/stock" element={<Stock />} />
         </Route>
     )
 )
