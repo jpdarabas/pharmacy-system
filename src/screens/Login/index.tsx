d696ea94b1f182525fa45e90eabc9a310f8d0f34
Paulo Cesar Dal Ponte <129686037+pauloDalponte@users.noreply.github.com>
2023-11-19 11:46:28 -0300

    atualizacao

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,10 +1,7 @@
 import { useState } from 'react';
-import { Button, TextField } from '@mui/material';
+import { Button, TextField, Typography, Grid, Link } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
-import LogoSvg from '../../assets/logo.png';
-
-// Importe a imagem de fundo
-import backgroundImage from '../../assets/farma_fundo.png';
+import LogoPng from '../../assets/logo_png.png';
 
 export default function Login() {
   const [username, setUsername] = useState('');
@@ -13,31 +10,19 @@ export default function Login() {
 
   const handleLoginInButton = () => {
     if (username && password) {
-      // Navegar para a rota /dashboard após o login bem-sucedido
+
       navigate('/dashboard');
     } else {
       alert('Por favor, preencha os campos de usuário e senha.');
     }
   };
 
-  const loginStyle: React.CSSProperties = {
-    backgroundImage: `url(${backgroundImage})`,
-    backgroundSize: 'cover',
-    backgroundPosition: 'center',
-    minHeight: '97vh',
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: '3px',
-  };
-
   return (
-    <Grid container justifyContent="center" alignItems="center" className="h-screen bg-purple-500">
-      <div className="bg-white p-8 rounded-lg shadow-md">
+    <Grid container justifyContent="center" alignItems="center" className="h-screen">
+      <div className="bg-purple-400 p-8 rounded-lg shadow-md">
         <Grid container spacing={2} direction="column" alignItems="center">
           <Grid item>
-            <img src={LogoSvg} alt="Logo" className="w-42 h-42" />
+            <img src={LogoPng} alt="Logo" className="w-32 h-32" />
           </Grid>
           <Grid item>
             <Typography variant="h5">Login</Typography>
@@ -48,7 +33,7 @@ export default function Login() {
               label="Usuário"
               value={username}
               onChange={(e) => setUsername(e.target.value)}
-              className="custom-textfield "
+              className="custom-textfield"
             />
           </Grid>
           <Grid item>
@@ -62,12 +47,12 @@ export default function Login() {
           </Grid>
           <Grid item>
             <Button variant="contained" color="primary" onClick={handleLoginInButton}>
-              Entrar
+              Login
             </Button>
           </Grid>
           <Grid item>
             <Link href="#" variant="body2">
-              Esqueceu sua senha?
+              Esqueceu a senha?
             </Link>
           </Grid>
         </Grid>
