9a4c30ed6865b8d964ee6b08b69cff9fe485526c
Bruno Rosso <110792695+Bruno01996@users.noreply.github.com>
2023-11-20 22:57:19 -0300

    tela pedidos
    Tela pedidos

--- src/Router/router.tsx
+++ src/Router/router.tsx
@@ -1,11 +1,13 @@
 import { Route, createBrowserRouter, createRoutesFromElements } from "react-router-dom";
 import Login from "../screens/Login";
+import Pedidos from "../screens/Pedidos"
 
 const Router = createBrowserRouter(
     createRoutesFromElements(
         <Route>
             <Route path="/" element={<Login />} />
-        </Route>
+            <Route path="/pedidos" element={<Pedidos />} />
+      </Route>
     )
 )
 
