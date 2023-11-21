211b66b9e4502f01fcda385ce2aeb61eb091b76a
Paulo Cesar Dal Ponte <129686037+pauloDalponte@users.noreply.github.com>
2023-11-21 19:40:02 -0300

    atualizacao tela estoque

--- src/screens/Stock/index.tsx
+++ src/screens/Stock/index.tsx
@@ -9,6 +9,30 @@ const Dashboard: React.FC<StockProps> = () => {
     <div className='ml-48'>
       <h1>Olá, você está na tela de estoque e cadastro!</h1>
       {/* Adicione qualquer conteúdo adicional que você queira exibir no dashboard */}
+      <br></br>
+      <input
+        type="text"
+        placeholder="Escolha o produto"
+      />
+      <br></br>
+      <br></br>
+      <table>
+        <tr>
+          <th>Código</th>
+          <th>Nome</th>
+          <th>Quantidade</th>
+        </tr>
+        <tr>
+          <td>1</td>
+          <td>Paracetamol</td>
+          <td>3</td>
+        </tr>
+        <tr>
+          <td>2</td>
+          <td>Dipirona</td>
+          <td>5</td>
+        </tr>
+      </table>
     </div>
   );
 };
